8e2b3d3e92360246ab16f4bd8a0f3623eda80d46
Alhassan-Jamila <jamilaal865@gmail.com>
2022-03-29 15:28:44 +0000

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import withLayout from './withLayout'
+
+function Home() {
+  return (
+    <div>
+    <h1>welcome to my Homepage</h1>
+    <div>
+    <p>10 Sept 2021 — WhatsApp Web is a free version of the famous chat/messaging app WhatsApp that will allow you to chat with your contacts from your computert default Home. </p>
+
+
+    <p>10 Sept 2021 — WhatsApp Web is a free version of the famous chat/messaging app WhatsApp that will allow you to chat with your contacts from your computert default Home. </p>
+
+
+    <p>10 Sept 2021 — WhatsApp Web is a free version of the famous chat/messaging app WhatsApp that will allow you to chat with your contacts from your computert default Home. </p>
+    </div>
+
+
+    </div>
+  )
+  }
+
+export default withLayout(Home)
+
