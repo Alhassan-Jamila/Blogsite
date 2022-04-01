746246a990595c7b0906d46abbdded9275211c5c
Alhassan-Jamila <jamilaal865@gmail.com>
2022-04-01 16:12:29 +0000

    added some data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,34 @@
 import React from 'react'
 import withLayout from './withLayout'
 
-function Home() {
+function home() {
   return (
     <div>
-    <h1>welcome to my Homepage</h1>
+    <h1>Welcome to my home page</h1>
     <div>
-    <p>10 Sept 2021 — WhatsApp Web is a free version of the famous chat/messaging app WhatsApp that will allow you to chat with your contacts from your computert default Home. </p>
+    <p>Kane was cast in The Fading Light by the director Ivan Kavanagh after he spotted her in a short film, 
+    July, that was posted on YouTube.[3] She was chosen partly for her successful experience with
+     improvisation in the short film.[4] 2013 saw her play Rose Stagg in the BBC's TV series The Fall,
+     and Dara in the comic Irish thriller Jump. Also in 2013, Kane played the title role in Strindberg's
+      Miss Julie at the newly founded Reading Rep.[5]</p>
 
-
-    <p>10 Sept 2021 — WhatsApp Web is a free version of the famous chat/messaging app WhatsApp that will allow you to chat with your contacts from your computert default Home. </p>
-
-
-    <p>10 Sept 2021 — WhatsApp Web is a free version of the famous chat/messaging app WhatsApp that will allow you to chat with your contacts from your computert default Home. </p>
+    <p>Other film work Still Early,
+     a short film which premiered at the Galway Film Festival. Kane's work for the BBC in 2016 
+     includes taking the lead in BBC Three drama Thirteen, the third series of The Fall, and an
+      episode of Murder. Also that year, she played Lyra Erso, the protagonist's mother,
+       in the film Rogue One: A Star Wars Story.[6][7]</p>
+    
+    <p>Kane has been seen on stage as Nance, in the Finborough Theatre's production of Autumn Fire
+    ,[8] The Love in Punchdrunk's production The Black Diamond,[9] which sold out "in mere minutes"
+    [10] and Lady Lydia Languish in The Rivals.[11] She also played Girleen in Martin McDonagh's
+     The Lonesome West in which one reviewer said "Kane gives Girleen a schoolgirl reality,
+      her confident swagger and challenge covering the only genuine feelings for anyone else 
+      that the play possesses"</p>
     </div>
 
-
     </div>
+    
   )
-  }
+}
 
-export default withLayout(Home)
-
+export default withLayout (home)
